a7ee2983863a0f2a30ff6e9a5ab13ef089a9244e
Ryan <naamloosdt@gmail.com>
2019-03-20 18:31:46 +0100

    Nicer site

--- include/script.js
+++ include/script.js
@@ -1,26 +1,36 @@
 // Functions
-var defaultdisplay = null;
 function setInitial(){
     // Initial state
-    defaultdisplay = document.getElementById("aboutsection").style.display;
-    document.getElementById("aboutsection").style.display = defaultdisplay;
+    document.getElementById("profilesection").style.display = 'block';
     document.getElementById("projectssection").style.display = 'none';
     document.getElementById("socialsection").style.display = 'none';
+
+    document.getElementById('notification').parentElement.parentElement.parentElement.style.display = 'none';
 
     document.getElementsByTagName("yesscript")[0].style.display = 'block';
 }
 
 function pagebutton(item){
     window.scrollTo(0, 0);
-    document.getElementById("about").classList.remove("is-active");
+    document.getElementById("profile").classList.remove("is-active");
     document.getElementById("projects").classList.remove("is-active");
     document.getElementById("social").classList.remove("is-active");
 
     item.classList.add("is-active");
 
-    document.getElementById("aboutsection").style.display = 'none';
+    document.getElementById("profilesection").style.display = 'none';
     document.getElementById("projectssection").style.display = 'none';
     document.getElementById("socialsection").style.display = 'none';
 
-    document.getElementById(item.id + "section").style.display = defaultdisplay;
+    document.getElementById(item.id + "section").style.display = 'block';
+}
+
+function closeNotif(item){
+    item.parentElement.parentElement.parentElement.style.display = 'none';
+}
+
+function showNotif(text){
+    var notif = document.getElementById('notification');
+    notif.parentElement.parentElement.parentElement.style.display = 'block';
+    notif.innerHTML = text;
 }
